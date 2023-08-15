a4ddade6040089aa157c1cb0957c94ac0ffc84e4
eftpmc <61393419+eftpmc@users.noreply.github.com>
2023-08-15 13:13:26 -0400

    vlc playback, shuffle and fade in and out

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import os
 from dotenv import load_dotenv
 import re
 import json
-import pygame
-import random
 from urllib import request, parse
 from pytube import YouTube
-from pydub import AudioSegment
-from pydub.playback import play
+import vlc
+import time
+import random
 
 # Load the environment variables from the .env file
 load_dotenv()
@@ -59,64 +58,79 @@ def download_audio(video_url, folder_path):
     
     if not os.path.exists(full_path):
         audio_file = audio_stream.download(output_path=folder_path)
-        base, ext = os.path.splitext(audio_file)
-        sound = AudioSegment.from_file(audio_file, format=ext.replace('.', ''))
-        sound.export(full_path, format='mp3')
-        
-        os.remove(audio_file)
         print(f"downloaded: {full_path}")
     else:
         print(f"file {full_path} already exists. skipping download.")
 
 class Player:
     def __init__(self, folder_path):
+        self.Instance = vlc.Instance()
+        self.player = self.Instance.media_player_new()
+        self.MediaList = self.Instance.media_list_new()
+        self.MediaListPlayer = self.Instance.media_list_player_new()
+        self.MediaListPlayer.set_media_list(self.MediaList)
+        self.MediaListPlayer.set_media_player(self.player)
+        
         self.folder_path = folder_path
-        self.play_list = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.mp3')]
+        self.original_play_list = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.mp3')]
+        self.play_list = self.original_play_list.copy()
         self.index = 0
-        self.shuffle = False
-        self.volume = 1  # Default volume (0.0 to 1.0)
+        self.is_shuffle_enabled = False
+        self.current_volume = 100
+        self.fade_duration = 0
+        self.player = vlc.MediaPlayer()
         print(self.play_list)
-
+        
     def play(self):
         if not self.play_list:
             print("No songs to play!")
             return
+        self.player.set_media(vlc.Media(self.play_list[self.index]))
+        self.player.play()
+        self.fade_in()
 
-        song = AudioSegment.from_file(self.play_list[self.index])
-        
-        # Apply fade in and fade out
-        song = song.fade_in(2000).fade_out(2000)  # 2 seconds fade in/out
-        
-        # Set volume
-        song = song - (1 - self.volume) * 50  # 50 here is an arbitrary number for volume control
-        
-        # Play the song
-        play(song)
-        
-    def set_volume(self, volume):
-        self.volume = max(0.0, min(1.0, volume))  # Ensure volume is between 0.0 and 1.0
-        print(f"Volume set to {self.volume * 100}%")
+    def fade_in(self):
+        fade_steps = int(self.fade_duration * 10)
+        for step in range(fade_steps):
+            time.sleep(self.fade_duration / fade_steps)
+            volume = int((step + 1) / fade_steps * self.current_volume)
+            self.player.audio_set_volume(volume)
 
+    def fade_out(self):
+        fade_steps = int(self.fade_duration * 10)
+        for step in range(fade_steps):
+            time.sleep(self.fade_duration / fade_steps)
+            volume = int((fade_steps - step - 1) / fade_steps * self.current_volume)
+            self.player.audio_set_volume(volume)
+        self.stop()
+
+    def set_volume(self, volume_level):
+        self.current_volume = int(volume_level * 100)
+        self.player.audio_set_volume(self.current_volume)
+        
     def stop(self):
-        # Simpleaudio doesn't provide a stop function, so we move to the next track to effectively stop the current one
-        self.skip()
+        self.player.stop()
+        print("Stopped")
 
     def skip(self):
+        self.fade_out()
         self.index += 1
         if self.index >= len(self.play_list):
             self.index = 0
         self.play()
 
     def enable_shuffle(self):
-        self.shuffle = True
         random.shuffle(self.play_list)
+        self.is_shuffle_enabled = True
+        print("Shuffle enabled")
 
     def disable_shuffle(self):
-        self.shuffle = False
-        self.play_list = sorted(self.play_list)
+        self.play_list = self.original_play_list.copy()
+        self.is_shuffle_enabled = False
+        print("Shuffle disabled")
 
     def toggle_shuffle(self):
-        if self.shuffle:
+        if self.is_shuffle_enabled:
             self.disable_shuffle()
         else:
             self.enable_shuffle()
@@ -151,7 +165,7 @@ if __name__ == "__main__":
     
     player = Player(playlist_folder)
     while True:
-        command = input("\nenter command (play, shuffle, stop, skip, volume, quit): ").strip().lower()
+        command = input("\nenter command (play, shuffle, stop, skip, volume, fade, quit): ").strip().lower()
         if command == "play":
             player.play()
         elif command == "shuffle":
@@ -162,7 +176,18 @@ if __name__ == "__main__":
             player.skip()
         elif command == "volume":
             volume_level = float(input("enter volume (0.0-1.0): "))
-            player.set_volume(volume_level)
+            if 0.0 <= volume_level <= 1.0:
+                player.set_volume(volume_level)
+                print(f"Volume set to {volume_level*100}%")
+            else:
+                print("Invalid volume level! Enter a value between 0.0 and 1.0.")
+        elif command == "fade":
+            fade_time = float(input("enter fade time (0.0-10.0): "))
+            if 0.0 <= fade_time <= 10.0:
+                player.fade_duration = fade_time
+                print(f"Fade time set to {fade_time} seconds.")
+            else:
+                print("Invalid fade time! Enter a value between 0.0 and 10.0 seconds.")
         elif command == "quit":
             player.stop()
             break
